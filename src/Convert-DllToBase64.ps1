a3f6808173fd12dc46603a1f49eb7c30af72a05c
Michael West <michaellwest@gmail.com>
2020-10-07 11:30:39 -0500

    Added script for converting dll to base64.

---
+++ src/Convert-DllToBase64.ps1
@@ -0,0 +1,16 @@
+﻿# https://powershell.one/tricks/assemblies/load-from-memory
+Clear-Host
+
+$scriptDirectory = & {
+    if ($psISE) {
+        Split-Path -Path $psISE.CurrentFile.FullPath        
+    } else {
+        $PSScriptRoot
+    }
+}
+
+$library = "$($scriptDirectory)\Unicorn.PowerShell\bin\Debug\Unicorn.PowerShell.dll"
+$libraryBytes = [System.IO.File]::ReadAllBytes($library)
+# turn bytes into Base64 string:
+$libraryBase64 = [System.Convert]::ToBase64String($libraryBytes)
+$libraryBase64 | Set-Clipboard
