7240cc2354bc39e4b4be8bbdeeebd8f1a3236f85
Michael West <michaellwest@gmail.com>
2020-10-07 11:22:20 -0500

    Added project containing the bulk install and extract code.

---
+++ src/Unicorn.PowerShell/BulkItemExtractor.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Rainbow.Filtering;
+using Rainbow.Storage.Yaml;
+using Sitecore.Data;
+using Sitecore.Data.Engines;
+using Sitecore.Data.Events;
+using Sitecore.SecurityModel;
+
+namespace Unicorn.PowerShell
+{
+    public static class BulkItemExtractor
+    {
+        private static IFieldFilter CreateFieldFilter()
+        {
+            // Updated to allow Revision to come back.
+            var config = @"<fieldFilter type=""Rainbow.Filtering.ConfigurationFieldFilter, Rainbow"" singleInstance=""true"">
+					<exclude fieldID=""{B1E16562-F3F9-4DDD-84CA-6E099950ECC0}"" note=""'Last run' field on Schedule template (used to register tasks)"" />
+					<exclude fieldID=""{52807595-0F8F-4B20-8D2A-CB71D28C6103}"" note=""'__Owner' field on Standard Template"" />
+					<exclude fieldID=""{F6D8A61C-2F84-4401-BD24-52D2068172BC}"" note=""'__Originator' field on Standard Template"" />
+					<exclude fieldID=""{D9CF14B1-FA16-4BA6-9288-E8A174D4D522}"" note=""'__Updated' field on Standard Template"" />
+					<exclude fieldID=""{BADD9CF9-53E0-4D0C-BCC0-2D784C282F6A}"" note=""'__Updated by' field on Standard Template"" />
+					<exclude fieldID=""{001DD393-96C5-490B-924A-B0F25CD9EFD8}"" note=""'__Lock' field on Standard Template"" />
+				</fieldFilter>";
+
+
+            var doc = new XmlDocument();
+            doc.LoadXml(config);
+
+            return new ConfigurationFieldFilter(doc.DocumentElement);
+        }
+
+        private static YamlSerializationFormatter CreateFormatter(IFieldFilter filter)
+        {
+            // shut yer gob again :D
+            var config = @"<serializationFormatter type=""Rainbow.Storage.Yaml.YamlSerializationFormatter, Rainbow.Storage.Yaml"" singleInstance=""true"">
+					<fieldFormatter type=""Rainbow.Formatting.FieldFormatters.MultilistFormatter, Rainbow"" />
+					<fieldFormatter type=""Rainbow.Formatting.FieldFormatters.XmlFieldFormatter, Rainbow"" />
+					<fieldFormatter type=""Rainbow.Formatting.FieldFormatters.CheckboxFieldFormatter, Rainbow"" />
+				</serializationFormatter>";
+
+            var doc = new XmlDocument();
+            doc.LoadXml(config);
+
+            return new YamlSerializationFormatter(doc.DocumentElement, filter);
+        }
+
+        private static string ProcessItemId(ID itemId)
+        {
+            if (itemId.IsNull) return null;
+            
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var item = db.GetItem(itemId);
+            if (item == null) return null;
+
+            var formatter = CreateFormatter(CreateFieldFilter());
+            
+            using (var stream = new MemoryStream())
+            {
+                formatter.WriteSerializedItem(new Rainbow.Storage.Sc.ItemData(item), stream);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static List<string> ItemExtractor (BlockingCollection<string> itemsToExtract, CancellationToken cancellationToken)
+        {
+            Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+
+            var yamlItems = new List<string>();
+            using (new BulkUpdateContext())
+            using (new EventDisabler())
+            using (new SecurityDisabler())
+            using (new SyncOperationContext())
+            {
+                while (!itemsToExtract.IsCompleted)
+                {
+                    if (!itemsToExtract.TryTake(out var itemId, -1))
+                    {
+                        break;
+                    }
+
+                    var yaml = ProcessItemId(ID.Parse(itemId));
+                    if (!string.IsNullOrEmpty(yaml))
+                    {
+                        yamlItems.Add(yaml);
+                    }
+                }
+            }
+
+            return yamlItems;
+        }
+
+        public static string[] LoadItems(string[] itemIds)
+        {
+            if (itemIds == null) return null;
+
+            var cancellationToken = new CancellationToken();
+            var itemsToExtract = new BlockingCollection<string>();
+            foreach (var itemId in itemIds)
+            {
+                itemsToExtract.Add(itemId, cancellationToken);
+            }
+            itemsToExtract.CompleteAdding();
+
+            var threads = itemsToExtract.Count > 3 ? 8 : 1;
+            var running = new List<Task<List<string>>>();
+            for (var i = 0; i < threads; i++)
+            {
+                running.Add(Task.Run(() => ItemExtractor(itemsToExtract, cancellationToken), cancellationToken));
+            }
+
+            var yamlItems = new List<string>();
+            foreach (var t in running)
+            {
+                t.Wait(cancellationToken);
+                yamlItems.AddRange(t.Result);
+            }
+
+            return yamlItems.ToArray();
+        }
+
+    }
+}
---
+++ src/Unicorn.PowerShell/BulkItemInstaller.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Rainbow.Filtering;
+using Rainbow.Model;
+using Rainbow.Storage.Sc.Deserialization;
+using Sitecore.Data;
+using Sitecore.Data.Engines;
+using Sitecore.Data.Events;
+using Sitecore.SecurityModel;
+using Unicorn.Deserialization;
+using Unicorn.Logging;
+
+namespace Unicorn.PowerShell
+{
+    public static class BulkItemInstaller
+    {
+        private static IFieldFilter CreateFieldFilter()
+        {
+            // Updated to allow Revision to come back.
+            var config = @"<fieldFilter type=""Rainbow.Filtering.ConfigurationFieldFilter, Rainbow"" singleInstance=""true"">
+					<exclude fieldID=""{B1E16562-F3F9-4DDD-84CA-6E099950ECC0}"" note=""'Last run' field on Schedule template (used to register tasks)"" />
+					<exclude fieldID=""{52807595-0F8F-4B20-8D2A-CB71D28C6103}"" note=""'__Owner' field on Standard Template"" />
+					<exclude fieldID=""{F6D8A61C-2F84-4401-BD24-52D2068172BC}"" note=""'__Originator' field on Standard Template"" />
+					<exclude fieldID=""{D9CF14B1-FA16-4BA6-9288-E8A174D4D522}"" note=""'__Updated' field on Standard Template"" />
+					<exclude fieldID=""{BADD9CF9-53E0-4D0C-BCC0-2D784C282F6A}"" note=""'__Updated by' field on Standard Template"" />
+					<exclude fieldID=""{001DD393-96C5-490B-924A-B0F25CD9EFD8}"" note=""'__Lock' field on Standard Template"" />
+				</fieldFilter>";
+
+
+            var doc = new XmlDocument();
+            doc.LoadXml(config);
+
+            return new ConfigurationFieldFilter(doc.DocumentElement);
+        }
+
+        private static bool ProcessItem(IItemData item)
+        {
+            if (item == null) return false;
+
+            var consoleLogger = new SitecoreLogger();
+
+            var deserializer = new DefaultDeserializer(false, new DefaultDeserializerLogger(consoleLogger), CreateFieldFilter());
+
+            consoleLogger.Info(item.Path);
+            deserializer.Deserialize(item, null);
+            return true;
+        }
+
+        private static int ItemInstaller(BlockingCollection<IItemData> itemsToInstall, CancellationToken cancellationToken)
+        {
+            Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+            var successCount = 0;
+            using (new BulkUpdateContext())
+            using (new EventDisabler())
+            using (new SecurityDisabler())
+            using (new SyncOperationContext())
+            {
+                while (!itemsToInstall.IsCompleted)
+                {
+                    if (!itemsToInstall.TryTake(out var item, -1))
+                    {
+                        break;
+                    }
+                    successCount += ProcessItem(item) ? 1 : 0;
+                }
+            }
+
+            return successCount;
+        }
+
+        public static int LoadItems(IItemData[] items)
+        {
+            if (items == null) return 0;
+
+            var cancellationToken = new CancellationToken();
+            var itemsToInstall = new BlockingCollection<IItemData>();
+            foreach (var item in items)
+            {
+                itemsToInstall.Add(item, cancellationToken);
+            }
+            itemsToInstall.CompleteAdding();
+
+            var threads = itemsToInstall.Count > 3 ? 8 : 1;
+            var running = new List<Task<int>>();
+            for (var i = 0; i < threads; i++)
+            {
+                running.Add(Task.Run(() => ItemInstaller(itemsToInstall, cancellationToken), cancellationToken));
+            }
+
+            var successCount = 0;
+            foreach (var t in running)
+            {
+                t.Wait(cancellationToken);
+                successCount += t.Result;
+            }
+
+            return successCount;
+        }
+    }
+}
