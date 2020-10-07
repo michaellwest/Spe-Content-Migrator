dc2035d7773bc919896536fcda50ad760f164334
Michael West <michaellwest@gmail.com>
2020-10-07 11:24:51 -0500

    Added scripts.

---
+++ src/scripts/MigrationSample.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+
+$scriptDirectory = & {
+    if ($psISE) {
+        Split-Path -Path $psISE.CurrentFile.FullPath        
+    } else {
+        $PSScriptRoot
+    }
+}
+. "$($scriptDirectory)\Copy-RainbowContent.ps1"
+
+$sourceSession = New-ScriptSession -user "Admin" -pass 'b' -conn "https://sourcesite.local"
+$destinationSession = New-ScriptSession -user "Admin" -pass 'b' -conn "https://destinationsite.local"
+
+$copyProps = @{
+    "CopyBehavior"="Overwrite"
+    "Recurse"=$true
+    "RemoveNotInSource"=$false
+    "Detailed"=$true
+    "CheckDependencies"=$false
+    "BoringMode"=$false
+}
+
+$copyProps["SourceSession"] = $sourceSession
+$copyProps["DestinationSession"] = $destinationSession
+# Default Home
+Copy-RainbowContent @copyProps -RootId "{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}"
